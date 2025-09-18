d6f6df6d11aaf369c55ab480c8519a3cad322e84
AlexandraG2411 <alexa.quintero2020@gmail.com>
2025-09-17 20:28:38 -0500

    Ejercicios terminados actividad1

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -3,7 +3,7 @@ package com.example;
 public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
-        // tu código aquí
+        
         int edad = 25;
         double altura = 1.75;
         String nombre = "Alexandra";
